be1a59cefcb7069e52f15bbb71a8e1240d84670b
Musti <mustafap4252@gmail.com>
2020-04-09 23:19:32 -0500

    mustafa den

--- src/test/java/GithubDeneme.java
+++ src/test/java/GithubDeneme.java
@@ -4,6 +4,6 @@ public class GithubDeneme {
     //Deneme
     //mustafa deneme
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("onur");
     }
 }
